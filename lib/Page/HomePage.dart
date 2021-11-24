f5d12db2ded652086635d14568848439f60a9f29
kasar_akash <technicalak282@gmail.com>
2021-11-24 15:11:42 +0530

    functions is created

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -41,6 +41,7 @@ class _HomePageState extends State<HomePage> {
       body: image.isEmpty
           ? SizedBox()
           : ListView.builder(
+              itemCount: image.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
                   height: 400,
